1e13cffa69255d1630662a38b193a75124927d61
Alex <alexfrankcodes@gmail.com>
2018-04-26 14:25:30 -0700

    Update App.js

--- App.js
+++ App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CustomHeader from './components/Header';
 import CustomFooter from './components/Footer';
-import SearchBar from './components/SearchBar';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={{backgroundColor: 'black', height: '100%'}}>
         <CustomHeader />
-        <SearchBar />
         <CustomFooter />
       </View>
     );
